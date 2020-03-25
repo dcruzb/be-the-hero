d4f157cf4f70750ebe4d1f5e41689e93f77f25b0
Daniel <dcb@cin.ufpe.br>
2020-03-25 14:00:03 -0300

    Migrations

---
+++ backend/src/database/migrations/20200325083821_create_ngos.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('ngos', function(table) {
+        table.string('id').primary()
+        
+        table.string('name').notNullable()
+        table.string('email').notNullable()
+        table.string('phone').notNullable()
+        table.string('whatsapp').notNullable()
+        table.string('city').notNullable()
+        table.string('state', 2).notNullable()
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTrable('ngo')
+};
