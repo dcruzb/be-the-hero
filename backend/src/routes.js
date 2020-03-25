57dfe086f38a47d32c9f6e6f6863503d56c6eeae
Daniel <dcb@cin.ufpe.br>
2020-03-25 14:40:20 -0300

    NGOs routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
-
+const crypto = require('crypto')
+const connection = require('./database/connection')
 
 routes.get('/', (request, response) => {
     return response.json({
@@ -9,15 +10,21 @@ routes.get('/', (request, response) => {
     })
 })
 
-routes.post('/users', (request, response) => {
-    const body = request.body
+routes.post('/ngos', async (request, response) => {
+    const { name, email, phone, whatsapp, city, state} = request.body
+    console.log({ name, email, phone, whatsapp, city, state})
 
-    console.log(body)
+    const id = crypto.randomBytes(4).toString('HEX')
 
-    return response.json({
-        app: 'Be the Hero!',
-        author: 'dcb@cin.ufpe.br'
-    })
+    await connection('ngos').insert({ id, name, email, phone, whatsapp, city, state})
+
+    return response.json({ id })
+})
+
+routes.get('/ngos', async (request, response) => {
+    const ngos = await connection('ngos').select('*')
+
+    return response.json(ngos)
 })
 
 module.exports = routes
