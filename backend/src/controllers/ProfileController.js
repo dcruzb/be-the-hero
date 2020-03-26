e72556a040ca67b7f248757307ced301b67b5b45
Daniel <dcb@cin.ufpe.br>
2020-03-25 21:21:56 -0300

    Cleaning create-react-app

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,7 +3,7 @@ const connection = require('../database/connection')
 module.exports = {
     async index(request, response) {
         const ngo_id = request.headers.authorization
-        
+
         const incidents = await connection('incidents')
             .where('ngo_id', ngo_id)
             .select('*')
